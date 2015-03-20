f4de590cd9367ddb0dda291aa61cfd525c20ca2b
andrewtso37 <atso@sfu.ca>
2015-03-19 18:26:21 -0700

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,9 @@
+angular.module('starter.controllers', [])
+
+.controller('NewGameCtrl', function($scope) {})
+
+.controller('TrackerCtrl', function($scope) {})
+
+.controller('TimerCtrl', function($scope) {})
+
+.controller('DiceCtrl', function($scope) {})
